aa4f60b4ba86e0055665e0fdd8af95dee74a5e90
Giorgi <grgpaikidze@gmail.com>
2025-10-04 19:46:06 +0400

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'events',
+    pathMatch: 'full',
+  },
+  {
+    path: 'events',
+    loadComponent: () =>
+      import('./components/event-list/event-list.component').then((m) => m.EventListComponent),
+  },
+  {
+    path: 'events/:id',
+    loadComponent: () =>
+      import('./components/event-details/event-details.component').then(
+        (m) => m.EventDetailsComponent
+      ),
+  },
+];
