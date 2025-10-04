4e1dd3103ac1e944885194eb311103741131e169
Giorgi <grgpaikidze@gmail.com>
2025-10-04 19:45:07 +0400

    feat: update app config with providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
+import { eventsReducer } from './store/events/events.reducer';
+import { betslipReducer } from './store/betslip/betslip.reducer';
+import { EventsEffects } from './store/events/events.effects';
+import { BetslipEffects } from './store/betslip/betslip.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore({
+      events: eventsReducer,
+      betslip: betslipReducer,
+    }),
+    provideEffects([EventsEffects, BetslipEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: false,
+    }),
+  ],
 };
