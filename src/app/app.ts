8124406735711f88997014f6691c51761f63ec1d
Giorgi <grgpaikidze@gmail.com>
2025-10-04 19:47:00 +0400

    feat: update app component with new components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { OddsService } from './services/odds.service';
+import { BetslipComponent } from './components/betslip/betslip.component';
 
 @Component({
   selector: 'sb-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, BetslipComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('sb');
+export class App implements OnInit {
+  private oddsService = inject(OddsService);
+
+  ngOnInit(): void {
+    // Start real-time odds simulation
+    this.oddsService.startOddsSimulation().subscribe();
+  }
 }
